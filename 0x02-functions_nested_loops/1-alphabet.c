5025ece4daa143a01b5921a6419ea9098958af6f
Ginny <prigen3@yahoo.com>
2023-08-15 18:55:30 -0700

    putcher

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+
+ * print_alphabet - print all alphabet in lowercase
+
+ */
+
+
+void print_alphabet(void)
+
+{
+
+        char e;
+
+
+        for (e = 'a'; e <= 'z'; e++)
+
+                _putchar(e);
+
+
+        _putchar('\n');
+
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char e;
+
+	for (e = 'a'; e <= 'z'; e++)
+	{
+		_putchar(e);
+	}
+	_putchar('\n');
+}
