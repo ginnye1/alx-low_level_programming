e6dfe67cce948476428d7318be7819d07c04bde8
Ginny <prigen3@yahoo.com>
2023-08-15 20:04:06 -0700

    putcher

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,16 @@
 #include "main.h"
 
-
 /**
-
  * print_alphabet - print all alphabet in lowercase
-
  */
 
-
 void print_alphabet(void)
-
 {
+	char e;
 
-        char e;
-
-
-        for (e = 'a'; e <= 'z'; e++)
-
-                _putchar(e);
-
-
-        _putchar('\n');
-
+	for (e = 'a'; e <= 'z'; e++)
+	{
+		_putchar(e);
+	}
+	_putchar('\n');
 }
