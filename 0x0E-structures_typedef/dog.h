81bfa52d6e7e143f188f9ada76af5d8545680f35
Ginny <prigen3@yahoo.com>
2023-09-12 19:52:32 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,46 +1,24 @@
-#include <stdlib.h>
-#include "dog.h"
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
- * new_dog - creates a new dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- * Return: pointer to a new dog
+ * struct dog - a struct that create a dog info
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: first struct with alx
  */
-dog_t *new_dog(char *name, float age, char *owner)
-{
-		unsigned int nl, ol, i;
-			dog_t *dog;
 
-			if (name == NULL || owner == NULL)
-				return (NULL);
-			dog = malloc(sizeof(dog_t));
-			if (dog == NULL)
-				return (NULL);
-			for (nl = 0; name[nl]; nl++)
-				;
-			nl++;
-			dog->name = malloc(nl * sizeof(char));
-			if (dog->name == NULL)
-			{
-				free(dog);
-				return (NULL);
-			}
-			for (i = 0; i < nl; i++)
-				dog->name[i] = name[i];
-			dog->age = age;
-			for (ol = 0; owner[ol]; ol++)
-				;
-			ol++;
-			dog->owner = malloc(ol * sizeof(char));
-			if (dog->owner == NULL)
-			{
-				free(dog->name);
-				free(dog);
-				return (NULL);
-			}
-			for (i = 0; i < ol; i++)
-				dog->owner[i] = owner[i];
-			return (dog);
-}
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
