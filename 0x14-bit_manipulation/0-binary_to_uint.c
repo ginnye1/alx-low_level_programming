e3cd21f108c58371a80bbdeef144f74e2b8480b7
Ginny <prigen3@yahoo.com>
2023-09-29 22:27:30 -0700

    fix 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,39 @@
 #include "main.h"
 /**
- * print_bi - print the binary representation of a number recursively
- * @n: the decimal input
- * Return: void
+ * binary_to_uint - a program that converts binary numbers
+ * to integers/decimal numbers
+ *
+ * @b: a character pointer to a string containing the
+ * binary numbers
+ *
+ * Return: the converted decimal (output)
  */
-void print_bi(unsigned long int n)
+unsigned int binary_to_uint(const char *b)
 {
-	if (n == 0)
-		return;
-	print_bi(n >> 1);
-	if ((n & 1) == 1)
-		_putchar('1');
-	if ((n & 1) == 0)
-		_putchar('0');
-}
-/**
- * print_binary - prints out binary
- * @n: decimal to be converted
- */
-void print_binary(unsigned long int n)
-{
-	if (n == 0)
-		_putchar('0');
-	else
+	unsigned int len;
+	size_t i = 0;
+	size_t j = 0;
+	size_t sum = 0;
+	size_t pow = 1;
+	int base = 2;
+
+	if (b == NULL)
+		return (0);
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	if (len == 1 && (b[0] == '0' || b[0] == '1'))
 	{
-		print_bi(n);
+		return (b[0] - 48);
 	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		return (0);
+		for (j = len - 1; j > 0; j--)
+		pow = pow * base;
+		sum = sum + (pow * (b[i] - 48));
+		len--;
+		pow = 1;
+	}
+	return (sum);
 }
